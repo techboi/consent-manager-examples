abaf887ed206eeb72033964526bdb16960f63615
Benedikt Rötsch <opensource@axe312.dev>
2021-03-17 00:53:07 +0100

    refactor(parcel): change integration configs to match v0.2

--- gatsby/src/components/consent-manager/wrapper.js
+++ gatsby/src/components/consent-manager/wrapper.js
@@ -26,7 +26,7 @@ const consentManagerConfig = {
       description:
         "Adds red borders around your content, demonstrates use of components that do e.g. click tracking",
       iconSrc: `data:image/svg+xml,%3csvg fill='none' stroke='black' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3e%3cpath stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9'%3e%3c/path%3e%3c/svg%3e`,
-      wrapperComponent: ({ children }) => (
+      WrapperComponent: ({ children }) => (
         <div style={{ border: "3px solid red" }}>{children}</div>
       ),
     },
--- parcel/index.tsx
+++ parcel/index.tsx
@@ -13,13 +13,17 @@ const consentManagerConfig: ConsentManagerConfig = {
     {
       id: 'video-platform',
       title: 'Video Inc.',
+      category: 'social',
+      Icon: () => <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z" /></svg>,
       description: 'Video Inc. is a popular service to share clips of cats.'
     },
     {
       id: 'integration-with-wrapper',
       title: 'Red Box Ltd.',
+      category: 'statistics',
+      Icon: () => <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" /></svg>,
       description: 'Adds red borders around your content, demonstrates use of components that do e.g. click tracking',
-      wrapperComponent: ({ children }) => (
+      WrapperComponent: ({ children }) => (
         <div style={{ border: '3px solid red' }}>{children}</div>
       ),
     },
